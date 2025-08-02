24ffaa381727acc9403bca45eb2935e839011ebf
MaitriSoni17 <maitrisoni171@gmail.com>
2025-08-02 10:05:47 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      {/* <UserState>
+        <NoteState> */}
+          <Router>
+            <Navbar />
+            {/* <Alert alert={alert} /> */}
+            <div className="container">
+              <Routes>
+                {/* <Route path="/" element={<Home showAlert={showAlert} />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login showAlert={showAlert} />} />
+                <Route path="/signup" element={<SignUp showAlert={showAlert} />} />
+                <Route path="/userdetails" element={<UserDetails showAlert={showAlert} />} /> */}
+              </Routes>
+            </div>
+          </Router>
+        {/* </NoteState>
+      </UserState> */}
+    </>
   );
 }
 
